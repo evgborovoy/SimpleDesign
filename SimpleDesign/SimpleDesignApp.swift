52a4eae57258c081ccb8edef6d7713ae66b97313
Evgeniy <borovoyevgen@gmail.com>
2024-11-26 22:31:05 +0400

    Initial Commit

---
+++ SimpleDesign/SimpleDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDesignApp.swift
+//  SimpleDesign
+//
+//  Created by Evgeniy Borovoy on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
